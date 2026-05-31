c55c5fb0302913cd03231492a04ba59edbbc3ef8
morgan11d <morkicel@gmail.com>
2026-05-31 09:35:49 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -5,12 +5,12 @@ Train::Train() : countOp(0), first(nullptr) {
 
 Train::~Train() {
     if (first == nullptr) return;
-    
+
     first->prev->next = nullptr;
-    
+
     Car* current = first;
     Car* temp = nullptr;
-    
+
     while (current != nullptr) {
         temp = current;
         current = current->next;
@@ -23,7 +23,7 @@ void Train::addCar(bool light) {
     newCar->light = light;
     newCar->next = nullptr;
     newCar->prev = nullptr;
-    
+
     if (first == nullptr) {
         first = newCar;
         newCar->next = newCar;
@@ -38,59 +38,59 @@ void Train::addCar(bool light) {
 
 int Train::getLength() {
     if (first == nullptr) return 0;
-    
+
     countOp = 0;
-    
+
     first->light = false;
-    
+
     Car* current = first->next;
     int count = 1;
     countOp++;
-    
+
     while (current->light == true) {
         count++;
         current = current->next;
         countOp++;
     }
-    
+
     current->light = true;
-    
+
     Car* checkpoint = current;
     for (int i = 0; i < count; i++) {
         current = current->prev;
         countOp++;
     }
-    
+
     if (current->light == false) {
         return count;
     }
-    
+
     while (true) {
         current = checkpoint->next;
         countOp++;
         int steps = 1;
-        
+
         while (current->light == true) {
             steps++;
             current = current->next;
             countOp++;
         }
-        
+
         count += steps;
-        
+
         current->light = true;
         checkpoint = current;
-        
+
         for (int i = 0; i < steps; i++) {
             current = current->prev;
             countOp++;
         }
-        
+
         for (int i = 0; i < count - steps; i++) {
             current = current->prev;
             countOp++;
         }
-        
+
         if (current->light == false) {
             return count;
         }
